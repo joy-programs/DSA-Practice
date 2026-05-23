92fbe00463f81ac7c3ab334b0a147f605eb42b1a
Joy Khandelwal <joykh.0581@gmail.com>
2026-05-24 00:00:13 +0530

    Update recursion.cpp

--- recursion.cpp
+++ recursion.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 using namespace std;
 
-//First type: Using a counter (No Backtracking)
+//First type: Using a counter, print 1 to N (No Backtracking)
 
-void f(int n, int counter) {
+void f1(int n, int counter) {
     if (counter > n) {
         return;
     }
     
     cout << counter << endl;
-    f(n, counter + 1);
+    f1(n, counter + 1);
     
     return;
 }
 
+// Second type: Using global variables, print N to 1 (Backtracking)
+void f2() {
+    f2();
+}
+
 int main() {
-    f(4, 1);
+    f1(4, 1);
     return 0;
 }
