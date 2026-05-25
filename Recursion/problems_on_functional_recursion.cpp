13ab5a5ac2304bd416c32ef12a119659e022863e
Joy Khandelwal <joykh.0581@gmail.com>
2026-05-25 16:42:21 +0530

    feat(recursion): Add problems on functional recursion
    Added some problems on functional recursion

---
+++ Recursion/problems_on_functional_recursion.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+// Swapping using a "for" or a "while" loop
+void f1(int arr[], int left, int len) {
+    int right = len - 1;
+    while (left < right) {
+        swap(arr[left], arr[right]);
+        left++;
+        right--;
+    }
+    return;
+}
+
+// MyWay: Swapping using two pointers (Recursion)
+void f2(int arr[], int l, int r) {
+    if (l >= r) {
+        return;
+    }
+    swap(arr[l], arr[r]);
+    f2(arr, l + 1, r - 1);
+    return;
+}
+
+//Swapping using single pointer (Recursion)
+void f3(int arr[], int i, int n) {
+    if (i >= n / 2) {
+        return;
+    }
+    swap(arr[i], arr[n - i - 1]);
+    f3(arr, i + 1, n);
+    return;
+}
+
+int main() {
+    int a[] = {1, 2, 3, 4, 5, 6};
+    for (int x : a) {
+        cout << x << ' ';
+    }
+    cout << endl;
+    int length = sizeof(a) / sizeof(a[0]);
+    f1(a, 0, length);
+    f2(a, 0, length - 1);
+    f3(a, 0, length);
+    for (int x : a) {
+        cout << x << ' ';
+    }
+}
