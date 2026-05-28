e713ed29d547d990b005bdcfd6b47b739693ae32
Joy Khandelwal <joykh.0581@gmail.com>
2026-05-28 22:52:46 +0530

    feat(Hashing): Solve two sum (without hashing)
    My initial approach to solving two sum.

---
+++ Hashing/twoSum.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//My Initial brute force method
+vector<int> twoSum(vector<int>& nums, int target) {
+        int size = nums.size();
+        for (int i = 0; i != size; i++) {
+            for (int j = i + 1; j != size; j++) {
+                if (nums[i] + nums[j] == target) {
+                    vector<int> k = {i, j};
+                    return k;
+                }
+            }
+        }
+        return {};
+    }
+    
+int main () {
+    vector<int> k = {1, 2, 3};
+    vector<int> m = twoSum(k , 5);
+    for (auto it:m) {
+        cout << it;
+    }
+    return 0;
+}
