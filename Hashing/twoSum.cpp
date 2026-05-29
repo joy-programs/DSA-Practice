3b1e6659bdd0436c48bf99c97a80e4b3e8f7eaa8
Joy Khandelwal <joykh.0581@gmail.com>
2026-05-29 21:25:52 +0530

    feat(Hashing): Sovle two sum (with Hashing)
    Best solution using unordered_map of hashing.

--- Hashing/twoSum.cpp
+++ Hashing/twoSum.cpp
@@ -2,19 +2,34 @@
 using namespace std;
 
 //My Initial brute force method
-vector<int> twoSum(vector<int>& nums, int target) {
-        int size = nums.size();
-        for (int i = 0; i != size; i++) {
-            for (int j = i + 1; j != size; j++) {
-                if (nums[i] + nums[j] == target) {
-                    vector<int> k = {i, j};
-                    return k;
-                }
+vector<int> bruteForce(vector<int>& nums, int target) {
+    int size = nums.size();
+    for (int i = 0; i != size; i++) {
+        for (int j = i + 1; j != size; j++) {
+            if (nums[i] + nums[j] == target) {
+                vector<int> k = {i, j};
+                return k;
             }
         }
-        return {};
     }
+    return {};
+}
     
+vector<int> bestSolution(vector<int>& nums, int target) {
+    unordered_map<int, int> mp;
+    int size = nums.size();
+
+    for (int i = 0; i < size; i++) {
+        int element = nums[i];
+        int complement = target - element;
+        if (mp.count(complement)) {
+            return {i, mp[complement]};
+        }
+        mp[element] = i;
+    }
+    return {};
+}
+
 int main () {
     vector<int> k = {1, 2, 3};
     vector<int> m = twoSum(k , 5);
